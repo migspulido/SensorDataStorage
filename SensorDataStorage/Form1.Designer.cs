cdfc097f7ece78a4cd243643e3c81462552d8cd1
root <root@localhost.localdomain>
2020-03-16 15:55:14 -0700

    First commit Message

---
+++ SensorDataStorage/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace SensorDataStorage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.zigCOMPort = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.OutputWindow = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // zigCOMPort
+            // 
+            this.zigCOMPort.FormattingEnabled = true;
+            this.zigCOMPort.Location = new System.Drawing.Point(24, 82);
+            this.zigCOMPort.Name = "zigCOMPort";
+            this.zigCOMPort.Size = new System.Drawing.Size(121, 21);
+            this.zigCOMPort.TabIndex = 0;
+            // 
+            // label10
+            // 
+            this.label10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(656, 16);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(176, 20);
+            this.label10.TabIndex = 27;
+            this.label10.Text = "Size of Sensor Data:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Blue;
+            this.label8.Location = new System.Drawing.Point(20, 16);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 20);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Serial COM";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(24, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 37);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "Open Port";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(89, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(59, 37);
+            this.button2.TabIndex = 30;
+            this.button2.Text = "Close Port";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // OutputWindow
+            // 
+            this.OutputWindow.BackColor = System.Drawing.SystemColors.MenuText;
+            this.OutputWindow.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputWindow.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.OutputWindow.Location = new System.Drawing.Point(166, 39);
+            this.OutputWindow.Name = "OutputWindow";
+            this.OutputWindow.Size = new System.Drawing.Size(490, 646);
+            this.OutputWindow.TabIndex = 31;
+            this.OutputWindow.Text = "";
+            this.OutputWindow.TextChanged += new System.EventHandler(this.OutputWindow_TextChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(162, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 20);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "All Sensor DATA";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(660, 39);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(606, 402);
+            this.webBrowser1.TabIndex = 33;
+            this.webBrowser1.Url = new System.Uri("C:\\IoTData", System.UriKind.Absolute);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(660, 444);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(284, 73);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "00:00:00";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(982, 444);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(284, 73);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "00:00:00";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(995, 520);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 37;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(1088, 520);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 35);
+            this.button4.TabIndex = 38;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(1180, 520);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 35);
+            this.button5.TabIndex = 39;
+            this.button5.Text = "Reset";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1278, 695);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutputWindow);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.zigCOMPort);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sensor Data Storage";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox zigCOMPort;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox OutputWindow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ SensorDataStorage/Form1.cs
@@ -0,0 +1,214 @@
+﻿//Miguel Pulido - Systems Architect
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+//using System.Threading;
+using System.IO.Ports;
+
+namespace SensorDataStorage
+{
+    public partial class Form1 : Form
+    {
+
+        //Initialize Timer
+
+        Timer t = new Timer();
+        
+        
+        // Use StringBuilder variable to capture any data received 
+        // through receivedData object
+        private StringBuilder receivedData = new StringBuilder();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Stop watch Variables
+        int hour, min, sec, ms = 0;
+
+        // Initial COM port scan and injection into combobox - zigCOMPort
+        // Setup event listener/handler for SerialPort object
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //timer interval
+            t.Interval = 1000; // in milliseconds
+            t.Tick += new EventHandler(this.t_Tick);
+
+            //start timer when form loads
+            t.Start(); // this will us t_Tick() method
+
+
+            
+            foreach (string portname in SerialPort.GetPortNames())
+            {
+                zigCOMPort.Items.Add(portname);
+            }
+            timer1.Start();
+        }
+
+
+        // Intialize Open Port button to open respective COM port
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = zigCOMPort.Text;
+            if (!serialPort1.IsOpen) serialPort1.Open();
+            
+            button1.Enabled = false;
+            button2.Enabled = true;
+           
+            
+            //Start timer to show elapse time for sensors data
+            timer2.Start();
+        
+        
+        }
+        
+        // Initialize Close Port button to close respective COM port
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen) serialPort1.Close();
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+                         
+            // This Section Assigns receivedData from Sensors to OutputWindow Text for display
+            OutputWindow.Text = receivedData.ToString();
+
+
+            // This Section outputs "OutputWindow.Text string to a file located C:\IoTData called IoTData.txt
+            // The True following the file will either append the file if true or create a new file each time launched if false
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\IoTData\IoTData.txt", false))
+            {
+                file.WriteLine(OutputWindow.Text);
+            }
+        }
+
+     
+     
+
+    //timer eventhandler for digital clock
+        private void t_Tick(object sender, EventArgs e)
+        {
+            //get current time
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+            int ss = DateTime.Now.Second;
+
+            //time routine
+            string time = "";
+
+            //padding leading zero
+            if (hh < 10)
+            {
+                time += "0" + hh;
+            }
+            else
+            {
+                time += hh;
+            }
+            time += ":";
+            if (mm < 10)
+            {
+                time += "0" + mm;
+            }
+            else
+            {
+                time += mm;
+            }
+            time += ":";
+            if (ss < 10)
+            {
+                time += "0" + ss;
+            }
+            else
+            {
+                time += ss;
+            }
+            //update digital clock lable
+            label2.Text = time;
+
+
+
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ms = 0;
+            sec = 0;
+            min = 0;
+            hour = 0;
+            label3.Text = 0 + ":" + 0 + ":" + 0 + ":" +0.ToString();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label3.Text = hour + ":" + min + ":" + sec + ":" + ms.ToString();
+            ms++;
+            if (ms > 10)
+            {
+                sec++;
+                ms = 0;
+            }
+            else
+            {
+                ms++;
+            }
+
+            if (sec > 59)
+            {
+                min++;
+                sec = 0;
+            }
+            if (min > 60)
+            {
+                hour++;
+                min = 0;
+            }
+                
+        }
+
+        // update our output window with the text that comes in from our COM ports
+        private void OutputWindow_TextChanged_1(object sender, EventArgs e)
+        {
+            //Setup up scrolling so it does not stay at the top while data is coming in
+
+            OutputWindow.SelectionStart = OutputWindow.Text.Length; // Set current caret position at the end
+            OutputWindow.ScrollToCaret(); // Now Scroll it automatically
+
+            OutputWindow.Refresh();
+
+
+            /////////////////works//////
+            receivedData.Append(serialPort1.ReadExisting());
+        }
+    
+    
+    
+    }
+}
